8636797ee25103145598ca4cdb5f794a94b48b6e
sazhar <saqibabdullahazhar@gmail.com>
2018-09-18 14:47:50 +0500

    updated content

--- TeqWays/TeqWays/Controllers/HomeController.cs
+++ TeqWays/TeqWays/Controllers/HomeController.cs
@@ -12,19 +12,11 @@ namespace TeqWays.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
+        
+        public string Contact(FormCollection fc)
         {
-            ViewBag.Message = "Your application description page.";
 
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return "OK";
         }
     }
 }
